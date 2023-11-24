ff35752b8a166725ad9e39956c884107bba96af4
Anna Mendrukh <amendrukh@gmail.com>
2023-11-24 15:37:06 +0200

    Directory Category has been changed.

--- src/components/Category/Categories.tsx
+++ src/components/Category/Categories.tsx
@@ -2,6 +2,7 @@ import {usePlayer} from "../../player/playerContext";
 import {useLocation, useNavigate} from "react-router-dom";
 import {useCategory} from "../../api/category";
 import Select from 'react-select';
+import AsyncSelect from "react-select/async";
 
 interface IOption {
     label: string;
@@ -17,34 +18,57 @@ export function Categories() {
     const navigate = useNavigate();
     const goQuiz = () => navigate("/quiz", {replace: true});
 
-    const [...options]: IOption[] = trivia_categories.map(item => ({
-        label: item.name,
-        value: item.id.toString()
-    }));
-
-    const defaultOption = options.map((item, index) => {
-        if (index === 0) {
-            return item.label
+    // const [...options]: IOption[] = trivia_categories.map(item => ({
+    //     label: item.name,
+    //     value: item.id.toString()
+    // }));
+    //
+    // const defaultOption = options.map((item, index) => {
+    //     if (index === 0) {
+    //         return item.label
+    //     }
+    // })
+    // console.log(defaultOption[0])
+    //
+    //
+    // const handleChange = (option: IOption | null) => {
+    //     if (option)
+    //         updateCategory(option?.value);
+    //
+    // }
+    //
+    // if (options.length === 0) {
+    //     return null;
+    // }
+    //
+    // return (
+    //     <>
+    //         <Select options={options} onChange={handleChange}/>
+    //         {isLogin &&
+    //             <button onClick={goQuiz}>Start game</button>}
+    //     </>
+    // )
+    if (trivia_categories.length === 0) {
+            return null;
         }
-    })
-    console.log(defaultOption[0])
 
+    const loadOption = (searchValue: string, callback: (options: IOption[]) => void) => {
+        const [...options]: IOption[] = trivia_categories.map(item => ({
+            label: item.name,
+            value: item.id.toString()
+        }));
 
-    const handleChange = (option: IOption | null) => {
-        if (option)
-            updateCategory(option?.value);
-
-    }
-
-    if (options.length === 0) {
-        return null;
+        const filterCategory = (inputValue: string) => {
+            return options.filter((i) =>
+                i.label.toLowerCase().includes(inputValue.toLowerCase())
+            );
+        };
+        callback(filterCategory(searchValue));
     }
 
     return (
         <>
-            <Select options={options} onChange={handleChange}/>
-            {isLogin &&
-                <button onClick={goQuiz}>Start game</button>}
+            <AsyncSelect loadOptions={loadOption} defaultOptions/>
         </>
     )
 }
